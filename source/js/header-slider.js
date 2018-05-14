5b3c169341cbe5b9ca18c4edb684d4b77824c519
Joe Park-Kennaby <jparkkennaby@gmail.com>
2018-05-14 23:22:37 +0100

    update js. wrap js in jquery wrapper and page load function. remove menuHover() from head

--- source/js/header-slider.js
+++ source/js/header-slider.js
@@ -1,60 +1,68 @@
-function headerSlider() {
+(function($) {
+    $(function() {
 
-	//first item is current
-	jQuery('#header-slider-content ul li:first').addClass('current');
+			function headerSlider() {
 
-	headerHeight = jQuery('#header').height();
-	jQuery('#header-slider-bg').css({ height: headerHeight });
+				//first item is current
+				jQuery('#header-slider-content ul li:first').addClass('current');
 
-	//first background images
-	var bgImg = jQuery('#header-slider-content ul li.current').children('.header-bg').text().split(',');
-	jQuery('#header-slider-bg').css({ background: '#'+bgImg[0] }).children('img').attr('src', bgImg[1]).addClass('current');
+				headerHeight = jQuery('#header').height();
+				jQuery('#header-slider-bg').css({ height: headerHeight });
 
-	var intSlide = 	setInterval( callNextHeaderSlide(), 10000);
+				//first background images
+				var bgImg = jQuery('#header-slider-content ul li.current').children('.header-bg').text().split(',');
+				jQuery('#header-slider-bg').css({ background: '#'+bgImg[0] }).children('img').attr('src', bgImg[1]).addClass('current');
+
+				var intSlide = 	setInterval( callNextHeaderSlide(), 10000);
 
 
-}
+			}
 
-function callNextHeaderSlide() {
+			function callNextHeaderSlide() {
 
-	var next = jQuery('#header-slider-content ul li.current').next();
-	var current = jQuery('#header-slider-content ul li.current');
+				var next = jQuery('#header-slider-content ul li.current').next();
+				var current = jQuery('#header-slider-content ul li.current');
 
-	if(next.length === 0) {
+				if(next.length === 0) {
 
-		next = jQuery('#header-slider-content ul li:first');
+					next = jQuery('#header-slider-content ul li:first');
 
-	}
+				}
 
-	current.stop().animate({ opacity: 0 }, 200, function() {
+				current.stop().animate({ opacity: 0 }, 200, function() {
 
-		current.removeClass('current').hide();
+					current.removeClass('current').hide();
 
-		next.css({ display: 'block', opacity: 0 }).stop().animate({ opacity: 1 }, 800, function() {
+					next.css({ display: 'block', opacity: 0 }).stop().animate({ opacity: 1 }, 800, function() {
 
-			next.addClass('current');
+						next.addClass('current');
 
-		});
+					});
 
-	headerHeight = jQuery('#header').height();
-	jQuery('#header-slider-bg').css({ height: headerHeight });
+				headerHeight = jQuery('#header').height();
+				jQuery('#header-slider-bg').css({ height: headerHeight });
 
-});
+			});
 
-	var bgImg = next.children('.header-bg').text().split(',');
-	jQuery('#header-slider-bg').append('<img src="'+bgImg[1]+'" alt="" style="display: none;" />');
-	jQuery('#header-slider-bg img.current').stop().animate({ opacity: 0 }, 200, function() {
+				var bgImg = next.children('.header-bg').text().split(',');
+				jQuery('#header-slider-bg').append('<img src="'+bgImg[1]+'" alt="" style="display: none;" />');
+				jQuery('#header-slider-bg img.current').stop().animate({ opacity: 0 }, 200, function() {
 
-		jQuery(this).remove();
+					jQuery(this).remove();
 
-	});
+				});
 
-	jQuery('#header-slider-bg').stop().animate({ 'backgroundColor': '#'+bgImg[0] }, 400);
+				jQuery('#header-slider-bg').stop().animate({ 'backgroundColor': '#'+bgImg[0] }, 400);
 
-	jQuery('#header-slider-bg img:not(.current)').fadeIn(800, function() {
+				jQuery('#header-slider-bg img:not(.current)').fadeIn(800, function() {
 
-		jQuery(this).addClass('current');
+					jQuery(this).addClass('current');
 
-	});
+				});
 
-}
+			}
+
+			headerSlider();
+
+			});
+})(jQuery);
--- source/js/navigation.js
+++ source/js/navigation.js
@@ -1,151 +1,159 @@
-function menuHovers() {
+(function($) {
+    $(function() {
 
-	//wrap H6 with spans
-	jQuery('#content h6').each(function() {
+			function menuHovers() {
 
-		jQuery(this).html('<span>'+jQuery(this).text()+'</span>');
+				//wrap H6 with spans
+				jQuery('#content h6').each(function() {
 
-	});
+					jQuery(this).html('<span>'+jQuery(this).text()+'</span>');
 
-	//main vars
-	var liLen = jQuery('#menu li:not(#menu li ul li)').length;
-	var startOpacity = (10 - liLen);
+				});
 
-	//sets the initial opacity and adds a class to them to indicate the initial opacity so we can access it later
-	var i = startOpacity;
-	var i2 = 1;
+				//main vars
+				var liLen = jQuery('#menu li:not(#menu li ul li)').length;
+				var startOpacity = (10 - liLen);
 
-	jQuery('#menu li:not(#menu li ul li)').each(function() {
+				//sets the initial opacity and adds a class to them to indicate the initial opacity so we can access it later
+				var i = startOpacity;
+				var i2 = 1;
 
-		jQuery(this).attr('id', 'menu_'+i2);
-		jQuery(this).prepend('<span></span><span class="hover"></span>');
-		jQuery(this).children('span').css({ opacity: '.'+i }).addClass(''+i);
-		i++; i2++;
+				jQuery('#menu li:not(#menu li ul li)').each(function() {
 
-	});
+					jQuery(this).attr('id', 'menu_'+i2);
+					jQuery(this).prepend('<span></span><span class="hover"></span>');
+					jQuery(this).children('span').css({ opacity: '.'+i }).addClass(''+i);
+					i++; i2++;
 
-	jQuery('#menu li ul li').each(function() {
+				});
 
-		jQuery(this).prepend('<span></span>');
+				jQuery('#menu li ul li').each(function() {
 
-	});
+					jQuery(this).prepend('<span></span>');
 
-	//hover effects
-	jQuery('#menu li:not(#menu li ul li)').hover(function() {
+				});
 
-		jQuery('#menu li.current span').stop().animate({ opacity: 0 }, 200);
+				//hover effects
+				jQuery('#menu li:not(#menu li ul li)').hover(function() {
 
-		var curTemp = jQuery(this).attr('id').split('_');
-		var current = curTemp[1];
-		var current1 = (current-1)+2;
+					jQuery('#menu li.current span').stop().animate({ opacity: 0 }, 200);
 
-		var pos = (liLen - current);
-		var initialOp = ((startOpacity - 1) + pos);
-    var initOp;
+					var curTemp = jQuery(this).attr('id').split('_');
+					var current = curTemp[1];
+					var current1 = (current-1)+2;
 
-		if(current > 1) {
+					var pos = (liLen - current);
+					var initialOp = ((startOpacity - 1) + pos);
+			    var initOp;
 
-			initOp = (startOpacity-2);
+					if(current > 1) {
 
-			for(i=current1; i<=liLen; i++) {
+						initOp = (startOpacity-2);
 
-				jQuery('#menu_'+i).children('span:not(.hover)').stop().animate({ opacity: '.'+initOp }, 200);
-				initOp++;
+						for(i=current1; i<=liLen; i++) {
+
+							jQuery('#menu_'+i).children('span:not(.hover)').stop().animate({ opacity: '.'+initOp }, 200);
+							initOp++;
+
+						}
+
+						var pos2 = (pos-1)+2;
+						initOp2 = (startOpacity-2)+pos2;
+
+						for(i=1;i<current;i++) {
+
+							jQuery('#menu_'+i).children('span:not(.hover)').stop().animate({ opacity: '.'+initOp2 }, 200);
+							initOp2++;
+
+						}
+
+					} else {
+
+						initOp = (startOpacity-2);
+
+						for(i=current1; i<=liLen; i++) {
+
+							jQuery('#menu_'+i).children('span:not(.hover)').stop().animate({ opacity: '.'+initOp }, 200);
+							initOp++;
+
+						}
+
+					}
+
+					jQuery(this).children('.hover').css({ display: 'block', opacity: 0 });
+					jQuery(this).children('.hover').stop().animate({ opacity: 1 }, 200);
+
+				}, function() {
+
+					jQuery('#menu li span.hover').stop().animate({ opacity: 0 }, 200);
+					var backOp2 = jQuery('#menu li.current span').attr('class');
+					jQuery('#menu li.current span').stop().animate({ opacity: '.'+backOp2 }, 400);
+
+				});
+
+				jQuery('#menu li ul li').hover(function() {
+
+					jQuery(this).children('span').css({ display: 'block', opacity: 0 }).stop().animate({ opacity: 1 }, 200);
+
+				}, function() {
+
+					jQuery(this).children('span').stop().animate({ opacity: 0 }, 200, function() {
+
+						jQuery(this).hide();
+
+					});
+
+				});
+
+				jQuery('#menu').children('li').hover(function() {
+
+					jQuery(this).children('ul:first').stop().css({ display: 'block', opacity: 0 }).animate({ opacity: 1 }, 400);
+
+				}, function() {
+
+					jQuery(this).children('ul:first').stop().animate({ opacity: 0 }, 400, function() {
+
+						jQuery(this).hide();
+
+					});
+
+				});
 
 			}
 
-			var pos2 = (pos-1)+2;
-			initOp2 = (startOpacity-2)+pos2;
 
-			for(i=1;i<current;i++) {
 
-				jQuery('#menu_'+i).children('span:not(.hover)').stop().animate({ opacity: '.'+initOp2 }, 200);
-				initOp2++;
+
+
+			function hideShowSubmenu() {
+
+				if($.cookie('show-submenu') == null) { $.cookie("show-submenu", true); }
+
+				if($.cookie('show-submenu') == 'false') { jQuery('#submenu').hide(); jQuery('#show-hide-submenu').addClass('show-submenu'); }
+
+				jQuery('#show-hide-submenu').click(function() {
+
+					if(jQuery(this).attr('class') == '') {
+
+						jQuery('#submenu').fadeOut(200);
+						jQuery(this).addClass('show-submenu');
+
+						$.cookie("show-submenu", false);
+
+					} else {
+
+						jQuery('#submenu').fadeIn(200);
+						jQuery(this).removeClass('show-submenu');
+
+						$.cookie("show-submenu", true);
+
+					}
+
+				});
 
 			}
 
-		} else {
-
-			initOp = (startOpacity-2);
-
-			for(i=current1; i<=liLen; i++) {
-
-				jQuery('#menu_'+i).children('span:not(.hover)').stop().animate({ opacity: '.'+initOp }, 200);
-				initOp++;
-
-			}
-
-		}
-
-		jQuery(this).children('.hover').css({ display: 'block', opacity: 0 });
-		jQuery(this).children('.hover').stop().animate({ opacity: 1 }, 200);
-
-	}, function() {
-
-		jQuery('#menu li span.hover').stop().animate({ opacity: 0 }, 200);
-		var backOp2 = jQuery('#menu li.current span').attr('class');
-		jQuery('#menu li.current span').stop().animate({ opacity: '.'+backOp2 }, 400);
-
-	});
-
-	jQuery('#menu li ul li').hover(function() {
-
-		jQuery(this).children('span').css({ display: 'block', opacity: 0 }).stop().animate({ opacity: 1 }, 200);
-
-	}, function() {
-
-		jQuery(this).children('span').stop().animate({ opacity: 0 }, 200, function() {
-
-			jQuery(this).hide();
+			  menuHovers();
 
 		});
-
-	});
-
-	jQuery('#menu').children('li').hover(function() {
-
-		jQuery(this).children('ul:first').stop().css({ display: 'block', opacity: 0 }).animate({ opacity: 1 }, 400);
-
-	}, function() {
-
-		jQuery(this).children('ul:first').stop().animate({ opacity: 0 }, 400, function() {
-
-			jQuery(this).hide();
-
-		});
-
-	});
-
-}
-
-
-
-
-
-function hideShowSubmenu() {
-
-	if($.cookie('show-submenu') == null) { $.cookie("show-submenu", true); }
-
-	if($.cookie('show-submenu') == 'false') { jQuery('#submenu').hide(); jQuery('#show-hide-submenu').addClass('show-submenu'); }
-
-	jQuery('#show-hide-submenu').click(function() {
-
-		if(jQuery(this).attr('class') == '') {
-
-			jQuery('#submenu').fadeOut(200);
-			jQuery(this).addClass('show-submenu');
-
-			$.cookie("show-submenu", false);
-
-		} else {
-
-			jQuery('#submenu').fadeIn(200);
-			jQuery(this).removeClass('show-submenu');
-
-			$.cookie("show-submenu", true);
-
-		}
-
-	});
-
-}
+})(jQuery);
